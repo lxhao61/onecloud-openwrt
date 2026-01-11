3649e93c6c9e4b15a480c1b7bc5d487bced6c010
haoran <lxhao61@gmail.com>
2026-01-12 05:00:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,5 +25,17 @@ sed -i 's/^\(.*99999\)/#&/' package/lean/default-settings/files/zzz-default-sett
 # 替换终端为bash
 #sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
 
+# 拉取 passwall-packages
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
+#cd package/passwall/packages
+#git checkout bc40fceb0488dfb5a4adb711cc1830a8021ee555
+#cd -
+
+# 拉取 luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
+#cd package/passwall/luci
+#git checkout ebd3355bdf2fcaa9e0c43ec0704a8d9d8cf9f658
+#cd -
+
 # 拉取 easytier、luci-app-easytier
 git clone https://github.com/EasyTier/luci-app-easytier.git package/easytier
