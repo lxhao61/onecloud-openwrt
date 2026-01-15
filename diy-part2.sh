3153c654c7a8603df1eaafadc816a71210a4ecdc
haoran <lxhao61@gmail.com>
2026-01-15 13:52:03 +0800

    Add files via upload

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.10.99/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
@@ -19,5 +19,45 @@ sed -i 's/192.168.1.1/192.168.10.99/g' package/base-files/files/bin/config_gener
 # Modify hostname
 sed -i 's/LEDE/OneCloud/g' package/base-files/files/bin/config_generate
 
+# 取消登陆密码
+sed -i 's/^\(.*99999\)/#&/' package/lean/default-settings/files/zzz-default-settings
+
 # 替换终端为bash
-sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
+#sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
+
+# 删除自带 geoview
+rm -rf feeds/packages/net/geoview
+rm -rf package/feeds/packages/geoview
+
+# 删除自带 chinadns-ng
+rm -rf feeds/packages/net/chinadns-ng
+rm -rf package/feeds/packages/chinadns-ng
+
+# 删除自带 v2ray-geodata
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf package/feeds/packages/v2ray-geodata
+
+# 删除自带 xray-core
+rm -rf feeds/packages/net/xray-core
+rm -rf package/feeds/packages/xray-core
+
+# 拉取 passwall-packages
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
+#cd package/passwall/packages
+#git checkout bc40fceb0488dfb5a4adb711cc1830a8021ee555
+#cd -
+
+# 删除自带 luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf package/feeds/luci/luci-app-passwall
+# 拉取 luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
+#cd package/passwall/luci
+#git checkout ebd3355bdf2fcaa9e0c43ec0704a8d9d8cf9f658
+#cd -
+
+# 删除自带 easytier
+rm -rf feeds/packages/net/easytier
+rm -rf package/feeds/packages/easytier
+# 拉取 easytier、luci-app-easytier
+git clone https://github.com/EasyTier/luci-app-easytier.git package/easytier
