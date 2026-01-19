b785edfab5d0b1f87ec8561dfedbcb07f2e8666f
haoran <lxhao61@gmail.com>
2026-01-20 00:10:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,34 +25,19 @@ sed -i 's/^\(.*99999\)/#&/' package/lean/default-settings/files/zzz-default-sett
 # 替换终端为bash
 #sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
 
-# 删除自带 geoview
-rm -rf feeds/packages/net/geoview
-rm -rf package/feeds/packages/geoview
-
-# 删除自带 chinadns-ng
-rm -rf feeds/packages/net/chinadns-ng
-rm -rf package/feeds/packages/chinadns-ng
-
-# 删除自带 v2ray-geodata
-rm -rf feeds/packages/net/v2ray-geodata
-rm -rf package/feeds/packages/v2ray-geodata
-
-# 删除自带 xray-core
-rm -rf feeds/packages/net/xray-core
-rm -rf package/feeds/packages/xray-core
-
+# 移除自带的核心库
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 # 拉取 passwall-packages
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
-#cd package/passwall/packages
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git package/passwall-packages
+#cd package/passwall-packages
 #git checkout bc40fceb0488dfb5a4adb711cc1830a8021ee555
 #cd -
 
-# 删除自带 luci-app-passwall
+# 移除过时的 luci 版本
 rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf package/feeds/luci/luci-app-passwall
-# 拉取 luci-app-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
-#cd package/passwall/luci
+# 拉取 passwall-luci
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall.git package/passwall-luci
+#cd package/passwall-luci
 #git checkout ebd3355bdf2fcaa9e0c43ec0704a8d9d8cf9f658
 #cd -
 
